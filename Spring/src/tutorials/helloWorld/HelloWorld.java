879d2e4d1bea7d62c065b41d5fa29e6405a2f6d0
igalna <igalna@gmail.com>
2015-08-22 15:56:26 +0100

    Spring Hello world

---
+++ Spring/src/tutorials/helloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package tutorials.helloWorld;
+
+public class HelloWorld {
+
+	private String message;
+	
+	public void getMessage() {
+		System.out.println("You're message : " + message);
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
