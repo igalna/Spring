50dabb16aeb5afa9faffe98228cf06214933f589
igalna <igalna@gmail.com>
2015-08-24 14:17:42 +0100

    Bean Definition Inheritance

--- Spring/src/tutorials/helloWorld/HelloWorld.java
+++ Spring/src/tutorials/helloWorld/HelloWorld.java
@@ -2,14 +2,24 @@ package tutorials.helloWorld;
 
 public class HelloWorld {
 
-	private String message;
+	private String message1;
+	private String message2;
 	
-	public void getMessage() {
-		System.out.println("You're message : " + message);
+	public void getMessage1() {
+		System.out.println("You're message : " + message1);
 	}
 	
-	public void setMessage(String message) {
-		this.message = message;
+	public void getMessage2() {
+		System.out.println("You're message : " + message2);
+	}
+	
+	
+	public void setMessage1(String message) {
+		this.message1 = message;
+	}
+	
+	public void setMessage2(String message) {
+		this.message2 = message;
 	}
 	
 	public void init() {
--- Spring/src/tutorials/helloWorld/Main.java
+++ Spring/src/tutorials/helloWorld/Main.java
@@ -11,7 +11,14 @@ public class Main {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		
 		HelloWorld objectA = (HelloWorld) context.getBean("helloWorld");
-		objectA.getMessage();
+		objectA.getMessage1();
+		objectA.getMessage2();
+		
+		HelloIndia objectB = (HelloIndia) context.getBean("helloIndia");
+		objectB.getMessage1();
+		objectB.getMessage2();
+		objectB.getMessage3();
+		
 		
 		((AbstractApplicationContext) context).registerShutdownHook();
 	}
