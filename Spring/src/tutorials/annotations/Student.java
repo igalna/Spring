a1bebe8ff46c4136698f864f27f3c66c6719a46a
igalna <igalna@gmail.com>
2015-08-25 23:24:31 +0100

    Student autowired(required=false)

--- Spring/src/tutorials/annotations/Student.java
+++ Spring/src/tutorials/annotations/Student.java
@@ -1,12 +1,13 @@
 package tutorials.annotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 
 public class Student {
 	private Integer age;
 	private String name;
 	
-	@Required
+	@Autowired(required=false)
 	public void setAge(Integer age) {
 		this.age = age;
 	}
@@ -15,7 +16,7 @@ public class Student {
 		return age;
 	}
 	
-	@Required
+	@Autowired
 	public void setName(String name) {
 		this.name = name;
 	}
