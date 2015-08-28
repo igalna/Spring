864efd1f547ae9c6e8649109cfc7a85603a70884
igalna <igalna@gmail.com>
2015-08-28 13:06:36 +0100

    Handling Custom Events

---
+++ Spring/src/tutorials/customEvents/CustomEvent.java
@@ -0,0 +1,16 @@
+package tutorials.customEvents;
+
+import org.springframework.context.ApplicationEvent;
+
+public class CustomEvent extends ApplicationEvent {
+
+	public CustomEvent(Object source) {
+		super(source);
+	}
+	
+	public String toString() {
+		return "My Custom Event";
+	}
+
+	
+}
